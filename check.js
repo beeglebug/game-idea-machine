9e2e8527e6680df6455839aa60628758e2759bf7
beeglebug <updownleftright@gmail.com>
2013-10-20 21:56:48 +0100

    added script which checks for dupes

---
+++ check.js
@@ -0,0 +1,33 @@
+var data = require('./data.js');
+
+console.log('checking for duplicates');
+
+for(type in data) {
+	for(set in data[type]) {
+		if(set == 'templates') { continue; }
+		var dupes = findDuplicates(data[type][set]);
+		if(dupes.length) {
+			console.log(type, ' -> ', set, ' -> ',  dupes);
+		}
+	}
+}
+
+
+function findDuplicates(arr) {
+  var len=arr.length,
+      out=[],
+      counts={};
+
+  for (var i=0;i<len;i++) {
+    var item = arr[i];
+    var count = counts[item];
+    counts[item] = counts[item] >= 1 ? counts[item] + 1 : 1;
+  }
+
+  for (var item in counts) {
+    if(counts[item] > 1)
+      out.push(item);
+  }
+
+  return out;
+}
